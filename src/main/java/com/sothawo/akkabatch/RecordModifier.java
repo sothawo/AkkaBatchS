87c4cbf48443a89a20b3ed8ed167f52ac06bbcc1
Peter-Josef Meisch <pj.meisch@sothawo.com>
2014-03-10 23:30:48 +0100

    RecordModifier Implementierung

---
+++ src/main/java/com/sothawo/akkabatch/RecordModifier.java
@@ -0,0 +1,67 @@
+package com.sothawo.akkabatch;
+
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.text.MessageFormat;
+
+/**
+ * Writer Actor.
+ *
+ * @author P.J. Meisch (pj.meisch@sothawo.com).
+ */
+public class RecordModifier extends UntypedActor {
+// ------------------------------ FIELDS ------------------------------
+
+    /** Logger */
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+    /** für die eigentliche Ausgabe */
+    private PrintWriter writer;
+
+// ------------------------ CANONICAL METHODS ------------------------
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof ProcessRecord) {
+            processRecord((ProcessRecord) message);
+        } else if (message instanceof InitWriter) {
+            initWriter((InitWriter) message);
+        } else {
+            unhandled(message);
+        }
+    }
+
+    /**
+     * Initialisiert den Writer.
+     *
+     * @param message
+     */
+    private void initWriter(InitWriter message) {
+        Boolean result = true;
+        try {
+            writer = new PrintWriter(message.getOutputFilename(), message.getEncoding());
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            log.error(e, "Initialisierung Writer");
+            result = false;
+        }
+        log.info(MessageFormat.format("Datei: {0}, Zeichensatz: {1}, Init-Ergebnis: {2}", message.getOutputFilename(),
+                                      message.getEncoding(), result));
+        sender().tell(new InitResult(result), getSelf());
+    }
+
+    /**
+     * verarbeitet den nächsten Datensatz.
+     *
+     * @param processRecord
+     *         Datensatz zum Schreiben.
+     */
+    private void processRecord(ProcessRecord processRecord) {
+        log.info("Datensatz Nr. " + processRecord.getRecordId());
+        // TODO: Dummycode
+        sender().tell(new WorkDone(), getSelf());
+    }
+}
