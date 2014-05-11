760b65913a2c264db9997d9e9ebb41c0ffb9c3f9
Peter-Josef Meisch <pj.meisch@sothawo.com>
2014-05-11 06:57:16 +0200

    class redesign

--- src/main/scala/com/sothawo/akkabatch/scala/DoWorkInfo.scala
+++ src/main/scala/com/sothawo/akkabatch/scala/DoWorkInfo.scala
@@ -7,14 +7,14 @@ import scala.beans.BeanProperty
  * Information about a DoWork message during processing.
  *
  * @param doWork the DoWork message
- * @param timestamp Timestamp of the last send
- * @param sendCount number of sending operations
  */
-class DoWorkInfo(@BeanProperty val doWork: DoWork, @BeanProperty var timestamp: Long,
-var sendCount: Int ) {
+class DoWorkInfo(@BeanProperty val doWork: DoWork) {
 
-  // TODO: ctor raus und default args im primary ctor, wenn kein Java mehr
-  def this(doWork: DoWork)  { this(doWork, System.currentTimeMillis(), 1) }
+  /** Timestamp of the last send */
+  @BeanProperty var timestamp: Long = System.currentTimeMillis()
+
+  /** number of sending operations */
+  var sendCount: Int = 1
 
   /**
    * sets the internal timestamp value and increases the sendcount value
