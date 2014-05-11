20c4f46be5ad0ecbcd261b462e370127fbc9fc26
Peter-Josef Meisch <pj.meisch@sothawo.com>
2014-05-11 06:56:59 +0200

    using Scala BigInt

--- src/main/scala/com/sothawo/akkabatch/scala/Fibonacci.scala
+++ src/main/scala/com/sothawo/akkabatch/scala/Fibonacci.scala
@@ -1,23 +1,21 @@
 package com.sothawo.akkabatch.scala
 
-import java.math.BigInteger
-
 /**
  * class to calculate the nth fibonacci-number.
  *
  * @author P.J. Meisch (pj.meisch@sothawo.com).
  */
 object Fibonacci {
-  def calculate(n: Int): BigInteger = {
+  def calculate(n: Int) = {
     if (0 >= n) {
       throw new IllegalArgumentException
     }
 
-    var prev = BigInteger.ONE
-    var fib = BigInteger.ONE
-    var tmp = BigInteger.ZERO
+    var prev = BigInt(1)
+    var fib = BigInt(1)
+    var tmp = BigInt(0)
     for (i <- 2 to n) {
-      tmp = fib.add(prev)
+      tmp = prev + fib
       prev = fib
       fib = tmp
     }
